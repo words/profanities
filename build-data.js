7a01b9df488114c38046e3a934008bd51dda46f8
wooorm <tituswormer@gmail.com>
2014-10-19 12:29:23 +0200

    Add pretty output to build-step

--- build-data.js
+++ build-data.js
@@ -16,4 +16,4 @@ data.forEach(function (word) {
     }
 });
 
-fs.writeFileSync('data/profanities.json', JSON.stringify(data));
+fs.writeFileSync('data/profanities.json', JSON.stringify(data, null, 2));

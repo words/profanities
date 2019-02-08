e78d9d9d4f40646c1580561b73519b5959eb688a
Titus Wormer <tituswormer@gmail.com>
2019-02-07 22:30:00 -0800

    Add tests for different languages

--- test.js
+++ test.js
@@ -3,9 +3,18 @@
 var test = require('tape')
 var profanities = require('.')
 
+var langs = ['ar-latn', 'es', 'fr', 'pt-br']
+
 test('profanities', function(t) {
   t.equal(typeof profanities, 'object', 'should be an array #1')
   t.equal(Array.isArray(profanities), true, 'should be an array #2')
   t.notEqual(profanities.indexOf('barf'), -1, 'should contain words')
+
+  langs.forEach(function(lang) {
+    t.doesNotThrow(function() {
+      require('./' + lang)
+    }, 'Should support ' + lang)
+  })
+
   t.end()
 })

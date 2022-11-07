fc1c9a676da2c5af8cbb0434036436e7b5629f35
Titus Wormer <tituswormer@gmail.com>
2022-11-07 10:25:09 +0100

    Add docs to types

--- build.js
+++ build.js
@@ -4,6 +4,9 @@
 
 import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
+import path from 'node:path'
+
+const languageNames = new Intl.DisplayNames(['en'], {type: 'language'})
 
 const cussUrl = new URL('node_modules/cuss/', import.meta.url)
 const pkgUrl = new URL('package.json', cussUrl)
@@ -18,17 +21,28 @@ let index = -1
 /* eslint-disable no-await-in-loop */
 while (++index < files.length) {
   const basename = files[index]
+
   assert(typeof basename === 'string', 'expected string exports in `cuss`')
   const modUrl = new URL(basename, cussUrl)
   /** @type {{cuss: Record<string, number>}} */
   const mod = await import(modUrl.href)
   const profanities = Object.keys(mod.cuss)
 
+  const stem = path.basename(basename, path.extname(basename))
+  const code = stem === 'index' ? 'en' : stem
+  const language = languageNames.of(code)
+
   await fs.writeFile(
     basename,
-    'export const profanities = ' + JSON.stringify(profanities, null, 2) + '\n'
+    '/**\n * List of lots of profane words: ' +
+      language +
+      '.\n */\nexport const profanities = ' +
+      JSON.stringify(profanities, null, 2) +
+      '\n'
   )
 
-  console.log('✓ ' + basename + ' (' + profanities.length + ')')
+  console.log(
+    '✓ ' + basename + ' (' + language + '; ' + profanities.length + ')'
+  )
 }
 /* eslint-enable no-await-in-loop */
